1bb3e14dbb3e6d6b6dc3a62c2ff5089c7c419db2
arundevn <arundevn@amdocs.com>
2020-05-14 12:00:10 +0530

    Java program update

--- Helloworld.java
+++ Helloworld.java
@@ -1,10 +1,10 @@
 public class Helloworld{
 	public static void main(String[] args){
 		for(int i=0; i<10; i++){
-System.out.println("Hello world.." + i);
-}
-	
+			System.out.println("Hello world.." + i);
+		}
+		System.out.println("It worked.. Hurray!!!!!!!");
 
-}
+	}
 
 }
